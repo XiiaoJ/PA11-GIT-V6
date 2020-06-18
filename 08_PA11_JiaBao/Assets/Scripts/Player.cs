35a92350280c24e2f861105662366934824e9445
XiiaoJ <64391002+XiiaoJ@users.noreply.github.com>
2020-06-18 19:29:25 +0800

    Updated by JiaBao
    Updated by JiaBao

---
+++ 08_PA11_JiaBao/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public int score = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if(transform.position.y >= 4)
+        {
+            transform.position = new Vector3(transform.position.x, 4, transform.position.z);
+        }
+        else if (transform.position.y <= -4)
+        {
+            transform.position = new Vector3(transform.position.x, -4, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Score")
+        {
+            score += 10;
+            scoreText.text = $"Score: {score}";
+        }
+    }
+}
